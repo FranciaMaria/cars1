69964cc6a7f60781119c18dd5ea8bcaa9059dff9
FranciaMaria <mariafrancia88@gmail.com>
2017-10-17 18:34:09 +0200

    task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { CarsComponent } from './components/cars/cars.component';
+
+const appRoutes:Routes = [
+ { path: 'cars', component: CarsComponent },
+ { path: '', redirectTo: '/', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+	)
   ],
+  exports: [
+    LayoutComponent
+	],
   providers: [],
   bootstrap: [AppComponent]
 })
